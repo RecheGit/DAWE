2f44cb1a249bde2cd7a33ed65ec03c5acf22a8d0
Reche20 <91413674+Reche20@users.noreply.github.com>
2023-04-12 16:57:13 +0200

    eliminar (not finished)

--- L9_LocalStorage/postits.js
+++ L9_LocalStorage/postits.js
@@ -9,6 +9,9 @@ function init() {
 	var button = document.getElementById("add_button");
 	button.onclick = createSticky;
 
+	var btnRemove = document.getElementById("remove_button");
+	btnRemove.onclick = clearStickyNotes;
+
 	console.log(localStorage.length);
 	console.log(localStorage);
 
@@ -19,7 +22,7 @@ function init() {
 		addStickyToDOM(textoPostit);
 		id=index;
 	}
-	//localStorage.clear();
+	localStorage.clear();
 	// cargar las notas postit de localStorage  
 	// cada nota se guarda como un par así: postit_X = texto_de_la_nota
 	// donde X es el número de la nota
@@ -33,7 +36,7 @@ function createSticky() {
         // crear la nota con nombre postit_X, donde X es un número entero
 	// (postit_1, postit_2, ...)  y guardarla en el localStorage
 	
-	id=id+1;
+	id=id+1; 	
 	addStickyToDOM(value);
 	localStorage.setItem("postit_"+id,value);
 	console.log(localStorage.length);
@@ -59,4 +62,22 @@ function clearStickyNotes() {
 	// obtener una referencia a la capa "stickies"
 	// recorrer los hijos (childNodes) de esa referencia,
 	// eliminándolos uno a uno (removeChild)
+	
+	var stickiesList = document.getElementById("stickies");
+	
+	while (stickiesList.firstChild) {
+		stickiesList.removeChild(stickiesList.firstChild);
+	
+	
+	}
+	for (let i = 1; i <= localStorage.length; i++) {
+		console.log("aaaaa");
+		localStorage.removeItem('postit_' + i);
+		console.log("DESPUES");
+		console.log(localStorage);
+		console.log("//////////");
+
+	}
+	// localStorage.clear();
+	console.log("Eliminados");
 }
